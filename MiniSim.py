01104453c2c558b4bf0ade1cbecd47e2f6bd4fc4
Francis Amadeo <36138122+fpamadeo@users.noreply.github.com>
2018-11-19 16:46:25 -0600

    Fixed hex to bin function

--- MiniSim.py
+++ MiniSim.py
@@ -1,33 +1,42 @@
 def hexToBin (line):
     newLine = 0
-    for i in (0, len(line)):
-        if(line[len(line) - i] == 'A'):
+    for i in range(0, len(line)):
+        print("Line: " + line[len(line) - i -1] + "\t# of chars:"+ str(len(line)))
+
+        if(line[len(line) - i -1] == 'A'):
             newLine += 10 * (16 ** i)
-        elif(line[len(line) - i] == 'B'):
+        elif(line[len(line) - i -1] == 'B'):
             newLine += 11 * (16 ** i)
-        elif (line[len(line) - i] == 'C'):
+        elif (line[len(line) - i -1] == 'C'):
             newLine += 12 * (16 ** i)
-        elif (line[len(line) - i] == 'D'):
+        elif (line[len(line) - i -1] == 'D'):
             newLine += 13 * (16 ** i)
-        elif (line[len(line) - i] == 'E'):
+        elif (line[len(line) - i -1] == 'E'):
             newLine += 14 * (16 ** i)
-        elif (line[len(line) - i] == 'F'):
+        elif (line[len(line) - i -1] == 'F'):
             newLine += 15 * (16 ** i)
         else:
-            newLine += line[len(line) - i] * (16**i)
-    return format(int(newLine), "32b")
+            newLine += int(line[len(line) - i -1]) * (16**i)
+    print(str(newLine))
+    newLine = format(int(newLine), "b")
+    return newLine.rjust(32, '0')
 
 
 def main():
     print("ECE366 Fall 2018: MIPS Mini Assembler")
-    print()
-    print("Members:")
+    print("\nMembers:")
     print("Francis Paul Amadeo")
     print("Steven Mac\n\n")
     print("Reading in instruction file:")
 
+    #START TEST
+    var = ["05","AB","AC", "19", "161"]
 
-
+    for i in range(0,5):
+        print (hexToBin(var[i]))
+    print("FINISHED")
+    #END TEST
+    exit()
 
 
 if __name__ == "__main__":
